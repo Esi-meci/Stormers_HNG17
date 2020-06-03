e10555196cf48e525c096a1cbae5794f44e37824
GIDEON PETERS <gideonpeters85@gmail.com>
2020-06-03 11:29:07 +0100

    added json query param check

--- index.php
+++ index.php
@@ -5,7 +5,6 @@ $path = $log_directory . '\scripts\*.*';
 
 $output = array();
 $res = array();
-$final_res = array();
 $fullnameRegex = "/\sis\s(.*)\swith\semail/";
 $idRegex = "/\sID\s(.*)\susing/";
 $languageRegex = "/\susing\s(.*)\sfor/";
@@ -25,20 +24,29 @@ foreach (glob($path) as $key => $file) {
     }
     $new['output'] = $output[$key];
 
+    //check status
     if (preg_match("$generalRegex", $new['output'])) {
         $new['status'] = 'Passed';
     } else {
         $new['status'] = 'Failed';
     }
+
+    //get fullname
     if (preg_match("$fullnameRegex", $output[$key], $matches1)) {
         $new['full name'] = $matches1[1];
     }
+
+    //get hng id
     if (preg_match("$idRegex", $output[$key], $matches1)) {
         $new['ID'] = $matches1[1];
     }
+
+    //get langugae
     if (preg_match("$languageRegex", $output[$key], $matches1)) {
         $new['language'] = $matches1[1];
     }
+
+    //get email 
     if (preg_match("$emailRegex", $output[$key], $matches1)) {
         $new['email'] = $matches1[1];
     }
@@ -46,6 +54,39 @@ foreach (glob($path) as $key => $file) {
     array_push($res, $new);
 }
 
+$final_res = json_encode($res, true);
 
-var_dump(json_encode($res, true));
-ob_flush(); //flusing the output stream
+
+if (isset($_GET["json"])) {
+    // $content = json_encode($content);
+    echo $final_res;
+    exit;
+} else {
+    var_dump($res);
+
+    ob_flush(); //flusing the output stream
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Team Storm</title>
+</head>
+
+<body>
+    <h1>Team Storm</h1>
+    <?php foreach ($res as $data) : ?>
+        <?php if (isset($data['error'])) : ?>
+            <p><?= "error" ?></p>
+        <?php else : ?>
+            <p><?= $data['output'] ?></?>
+            <?php endif; ?>
+        <?php endforeach; ?>
+
+</body>
+
+</html>
